6bc62594d2dfa8da690875812f281e2ec3d1a91d
ZTI <zhaotianff@163.com>
2020-08-18 23:11:13 +0800

    create repo

---
+++ crawler-line/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Reflection;
+
+namespace crawler_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand();
+
+            //添加一个打印版本的Command
+            var versionCommand = new Command("--version");
+            versionCommand.AddAlias("-v");
+
+            //添加到RootCommand
+            rootCommand.AddCommand(versionCommand);
+          
+            rootCommand.Description = ".net core command-line crawler";
+
+            //RootCommand执行的操作
+            rootCommand.Handler = CommandHandler.Create(() =>
+            {
+                PrintUsageInfo();
+            });
+
+            //打印版本Command执行的操作
+            versionCommand.Handler = CommandHandler.Create(PrintVersion);
+
+            // Parse the incoming args and invoke the handler
+            rootCommand.InvokeAsync(args).Wait();
+        }
+
+        static void PrintVersion()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var versionStr = assembly.GetName().Version.ToString();
+            var appName = assembly.GetName().Name;
+            Console.WriteLine($"{appName} - {versionStr}");
+        }
+
+        static void PrintUsageInfo()
+        {
+            Console.WriteLine(".net core command-line crawler.");
+        }
+    }
+}
---
+++ crawler-line/Util/RegexUtil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crawler_line.Util
+{
+    class RegexUtil
+    {
+    }
+}
